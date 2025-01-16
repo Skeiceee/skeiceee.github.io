c169037d9f197d6f063599641fa89a09b2dedaed
Victor Nunez <victoralejandronunezt@gmail.com>
2025-01-15 22:05:47 -0300

    Se hace un force theme a dark.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter } from "react-router"
 import AppRouter from "./router/AppRouter"
+import { ColorModeProvider } from "./components/ui/color-mode"
 
 function App() {
 
   return (
     <BrowserRouter>
-      <AppRouter/>
+      <ColorModeProvider forcedTheme="dark">
+        <AppRouter/>
+      </ColorModeProvider> 
     </BrowserRouter>
   )
 }
