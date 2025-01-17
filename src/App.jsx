a1e0bdb590b99eab6a1eb6f86ca96f7b9510b198
Victor Nunez <victoralejandronunezt@gmail.com>
2025-01-17 01:43:11 -0300

    importacion innecesaria

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter } from "react-router"
 import AppRouter from "./router/AppRouter"
 import { ColorModeProvider } from "./components/ui/color-mode"
-import { Box } from "@chakra-ui/react"
 
 function App() {
 
